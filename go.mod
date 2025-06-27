12d8017d61e677848f3c3afcf9a94350fdcf86a8
Lee Dale <leedale@proton.me>
2025-06-27 18:19:42 +0100

    Updates

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ssesuk/mhhs/simulator
 
 go 1.23.0
+
+require golang.org/x/crypto v0.39.0 // indirect
--- services/request-service.go
+++ services/request-service.go
@@ -5,6 +5,7 @@ import (
 	"crypto"
 	"crypto/rsa"
 	"crypto/sha256"
+	"crypto/tls"
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/pem"
@@ -16,6 +17,8 @@ import (
 	"ssesuk/mhhs/simulator/domain"
 	"strings"
 	"time"
+
+	"golang.org/x/crypto/pkcs12"
 )
 
 type authenticationParameters struct {
@@ -34,13 +37,14 @@ func NewService(request *domain.Request) *requestService {
 }
 
 func (service *requestService) ExecuteRequest() string {
-	url := fmt.Sprintf("%s/requests", service.request.BaseUrl)
-	requestVerb, requestDestination := "POST", "/requests"
+	url := service.request.BaseUrl
+	requestVerb := "POST"
 	messageBody := service.getMessageBody()
 	messageBodyString := string(messageBody)
+
 	currentTime := time.Now()
-	date := currentTime.UTC().Format("YYYY-MM-DDThh:mm:ssZ")
-	//date := "2021-04-20T15:00:00Z"
+	date := currentTime.UTC().Format("2006-01-02T15:04:05.000Z")
+	//date := "2025-06-27T14:29:09.061Z"
 
 	validEncodedCertificate, err := service.getEncodedCertificate()
 
@@ -48,17 +52,41 @@ func (service *requestService) ExecuteRequest() string {
 		return "Error:" + err.Error()
 	}
 
-	authenticationParameters, err := service.getRequestAuthenticationParameters(messageBodyString, requestVerb, requestDestination, date, validEncodedCertificate)
+	authenticationParameters, err := service.getRequestAuthenticationParameters(messageBodyString, requestVerb, url, date, validEncodedCertificate)
 
 	req, err := http.NewRequest(requestVerb, url, bytes.NewBuffer(messageBody))
 	req.ContentLength = int64(len(messageBody))
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("X-DIP-Signature", authenticationParameters.Signature)
 	req.Header.Set("X-DIP-Signature-Date", authenticationParameters.Date)
-	req.Header.Set("X-DIP-Certificate", authenticationParameters.Certificate)
+	req.Header.Set("X-DIP-Signature-Certificate", authenticationParameters.Certificate)
 	req.Header.Set("X-DIP-Content-Hash", authenticationParameters.MessageHash)
+	req.Header.Set("User-Agent", "PostmanRuntime/7.44.1")
 
-	client := &http.Client{}
+	pfxData, err := os.ReadFile("Certificate_Sureserve.pfx")
+	if err != nil {
+		return "Failed to read PFX file: " + err.Error()
+	}
+
+	privateKey, cert, err := pkcs12.Decode(pfxData, "495badac-daf0-4bed-ab4b-8e164b4a566f")
+	if err != nil {
+		return "Failed to decode PFX: " + err.Error()
+	}
+
+	tlsCert := tls.Certificate{
+		Certificate: [][]byte{cert.Raw},
+		PrivateKey:  privateKey,
+		Leaf:        cert,
+	}
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			Renegotiation: tls.RenegotiateOnceAsClient,
+			Certificates:  []tls.Certificate{tlsCert},
+		},
+	}
+
+	client := &http.Client{Transport: tr}
 	resp, err := client.Do(req)
 
 	if err != nil {
@@ -68,7 +96,8 @@ func (service *requestService) ExecuteRequest() string {
 	defer resp.Body.Close()
 
 	if resp.Status != "200 OK" {
-		return resp.Status
+		errorBody, _ := io.ReadAll(resp.Body)
+		return resp.Status + " " + string(errorBody)
 	}
 
 	// Read and print the response
@@ -110,13 +139,13 @@ func (service *requestService) getPrivateKey() (*rsa.PrivateKey, error) {
 		return nil, errors.New("could not decode private key certificate")
 	}
 
-	key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	key, err := x509.ParsePKCS8PrivateKey(block.Bytes)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return key, nil
+	return key.(*rsa.PrivateKey), nil
 }
 
 func (service *requestService) getRequestAuthenticationParameters(messageBody, requestVerb, requestDestination, date, encodedCertificate string) (*authenticationParameters, error) {
@@ -128,7 +157,7 @@ func (service *requestService) getRequestAuthenticationParameters(messageBody, r
 	signatureStringBuilder := strings.Builder{}
 	signatureStringBuilder.WriteString(requestVerb)
 	signatureStringBuilder.WriteString(";")
-	signatureStringBuilder.WriteString(requestDestination)
+	signatureStringBuilder.WriteString(strings.ToLower(requestDestination))
 	signatureStringBuilder.WriteString(";")
 	signatureStringBuilder.WriteString(date)
 	signatureStringBuilder.WriteString(";")
@@ -170,6 +199,9 @@ func (service *requestService) getEncodedCertificate() (string, error) {
 		return "", err
 	}
 
-	encodedCertificate := base64.StdEncoding.EncodeToString(publicKey)
+	keyString := string(publicKey)
+	keyString = strings.ReplaceAll(keyString, "\r\n", "\n")
+	encodedCertificate := base64.StdEncoding.EncodeToString([]byte(keyString))
+
 	return encodedCertificate, nil
 }
